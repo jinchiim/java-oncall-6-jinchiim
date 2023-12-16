d9c4bba8e741d1abca27ce9ae5c847dd87b974df
jinchiim <nellroll@syuin.ac.kr>
2023-12-16 18:02:25 +0900

    feat(Application.md): application 구현
    - 의존성 주입 및 구현

--- src/main/java/oncall/Application.java
+++ src/main/java/oncall/Application.java
@@ -1,7 +1,15 @@
 package oncall;
 
+
+import oncall.controller.OnCallController;
+import oncall.view.input.InputView;
+import oncall.view.output.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        new OnCallController(inputView, outputView).run();
     }
 }
